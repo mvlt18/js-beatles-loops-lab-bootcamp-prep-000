bb782fecf8bc23cbab26daeaff34cdecccab568d
Ella Taber <michaella.latkovic@gmail.com>
2019-02-04 00:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  var array = []
+  var array = [];
   for (var i=0;i<musicians.length;i++){
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-   return array
+   return array;
 }
 
 function johnLennonFacts(facts){
@@ -16,3 +16,5 @@ function johnLennonFacts(facts){
   }
   return array;
 }
+
+function 
